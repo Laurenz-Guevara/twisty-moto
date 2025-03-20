f150ded35af5732514e4d263fa9e0d148c954ad0
Laurenz Guevara <laurenzguevara@outlook.com>
2025-03-20 00:30:50 +0000

    feat: Add Auth0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <div>
       <h1>Twisty Moto</h1>
       <Button>Sign Up</Button>
-      <Button>Login</Button>
+      <Button asChild>
+        <Link href="/api/auth/login">Login</Link>
+      </Button>
+      <Button asChild>
+        <Link href="/api/auth/logout">Logout</Link>
+      </Button>
+      {user && (
+        <div>
+          <img src={user.picture as string} alt={user.name as string} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
     </div>
   );
 }
