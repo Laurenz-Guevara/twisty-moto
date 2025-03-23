dba61b0f5582e551ccba970fe0fdd06244ddf031
Laurenz Guevara <laurenzguevara@outlook.com>
2025-03-23 19:22:52 +0000

    feat: Display icon when authed

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useUser } from "@auth0/nextjs-auth0/client";
 import { Bell, LogOut, Settings, User } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -10,6 +11,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
@@ -17,9 +19,17 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function NavigationBar() {
   const { setTheme } = useTheme();
+  const { user } = useUser();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (user) setIsLoggedIn(true);
+    else setIsLoggedIn(false);
+  }, [user]);
 
   return (
     <header className="border-b bg-background z-10">
@@ -31,7 +41,6 @@ export default function NavigationBar() {
             </span>
           </Link>
         </div>
-
         <nav className="flex items-center space-x-4 lg:space-x-6 mx-6">
           <Link
             href={"/discover"}
@@ -72,9 +81,16 @@ export default function NavigationBar() {
         </nav>
 
         <div className="ml-auto flex items-center space-x-4">
-          <Button variant="ghost" className="hover:cursor-pointer" size="icon">
-            <Bell className="h-5 w-5" />
-          </Button>
+          {isLoggedIn &&
+            (
+              <Button
+                variant="ghost"
+                className="hover:cursor-pointer"
+                size="icon"
+              >
+                <Bell className="h-5 w-5" />
+              </Button>
+            )}
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button
@@ -99,47 +115,86 @@ export default function NavigationBar() {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-                <Avatar className="h-8 w-8">
-                  <AvatarImage
-                    src="/placeholder.svg?height=32&width=32"
-                    alt="User Avatar"
-                  />
-                  <AvatarFallback>JD</AvatarFallback>
-                </Avatar>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end" forceMount>
-              <DropdownMenuLabel className="font-normal">
-                <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">John Doe</p>
-                  <p className="text-xs leading-none text-muted-foreground">
-                    john.doe@example.com
-                  </p>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <User className="mr-2 h-4 w-4" />
-                <span>Profile</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Settings className="mr-2 h-4 w-4" />
-                <span>Settings</span>
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <Button variant="ghost" asChild>
-                  <span>
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <Link href="/api/auth/logout">Logout</Link>
-                  </span>
-                </Button>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          {isLoggedIn
+            ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="relative h-8 w-8 rounded-full hover:cursor-pointer"
+                  >
+                    <Avatar className="h-8 w-8">
+                      <AvatarImage
+                        src="/placeholder.svg?height=32&width=32"
+                        alt="User Avatar"
+                      />
+                      <AvatarFallback>
+                        {user && user.name && user.name[0] !== undefined
+                          ? user.name[0]
+                          : "?"}
+                      </AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end" forceMount>
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      {user && user.name && user.name !== undefined
+                        ? (
+                          <p className="text-sm font-medium leading-none">
+                            {user?.name}
+                          </p>
+                        )
+                        : (
+                          <Skeleton className="w-[100px] h-[20px] rounded-full" />
+                        )}
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <Link href="/profile">
+                    <DropdownMenuItem className="hover:cursor-pointer">
+                      <User className="mr-2 h-4 w-4" />Profile
+                    </DropdownMenuItem>
+                  </Link>
+                  <Link href="/setting">
+                    <DropdownMenuItem className="hover:cursor-pointer">
+                      <Settings className="mr-2 h-4 w-4" />
+                      <span>Settings</span>
+                    </DropdownMenuItem>
+                  </Link>
+                  <DropdownMenuSeparator />
+                  <Link href="/api/auth/logout">
+                    <DropdownMenuItem className="hover:cursor-pointer">
+                      <LogOut className="mr-2 h-4 w-4" />
+                      Logout
+                    </DropdownMenuItem>
+                  </Link>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )
+            : (
+              <>
+                <Link
+                  href={"/api/auth/login"}
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Login
+                </Link>
+
+                <Link
+                  href={"/api/auth/sign-up"}
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign Up
+                </Link>
+              </>
+            )}
         </div>
       </div>
     </header>
