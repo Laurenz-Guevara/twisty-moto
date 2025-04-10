e365f4c084ddbe255a9d7d6bc6c663c95525f3e0
Laurenz Guevara <laurenzguevara@outlook.com>
2025-04-10 02:02:10 +0100

    refactor: Reference to map is now useRef and allows for on listeners

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,29 +1,43 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
-import mapboxgl from "mapbox-gl";
+import React, { useEffect, useRef, useState } from "react";
+import mapboxgl, { LngLatLike } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
-
 const Map = () => {
-  const mapContainerRef = useRef(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+
+  const [center, setCenter] = useState<LngLatLike>([-1, 51]);
+  const [zoom, setZoom] = useState(7.8);
 
   useEffect(() => {
-    console.log("hi", mapContainerRef);
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current!,
-      center: [-1, 51],
-      attributionControl: true,
-      zoom: 7.8,
-    });
+    if (mapContainerRef.current) {
+      mapboxgl.accessToken = process.env
+        .NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
 
-    const resizeMap = () => {
-      if (map) setTimeout(() => map.resize(), 700);
-    };
-    resizeMap();
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        // style: "mapbox://styles/mapbox/dark-v11",
+        center: center,
+        zoom: zoom,
+      });
 
-    return () => map.remove();
+      mapRef.current.on("mousedown", (e) => {
+        console.log("Clicking");
+        console.log("event type:", e);
+      });
+
+      mapRef.current.on("drag", (e) => {
+        console.log("event type:", e.type);
+      });
+
+      return () => {
+        if (mapRef.current) {
+          mapRef.current.remove();
+        }
+      };
+    }
   }, []);
 
   return (
