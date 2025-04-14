8b08eadc1fee43c9458101b2159a02a7e14f3a8d
Laurenz Guevara <laurenzguevara@outlook.com>
2025-04-14 01:57:53 +0100

    feat: Add image to route editor

--- src/components/sidebar-header-content.tsx
+++ src/components/sidebar-header-content.tsx
@@ -1,19 +1,28 @@
 import { SidebarMenu, SidebarMenuItem } from "@/components/ui/sidebar";
 
-import { GalleryVerticalEnd } from "lucide-react";
 import Link from "next/link";
-
+import Image from "next/image";
 // TODO: Check if member from database
 
 export function SidebarHeaderContent() {
   return (
     <SidebarMenu>
       <SidebarMenuItem className="flex space-x-2 p-2 items-center">
-        <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-          <GalleryVerticalEnd className="size-4" />
+        <div className="size-12">
+          <Link className="" href="/">
+            <Image
+              src={"/twisty-moto.png"}
+              alt={"Twisty Moto Logo"}
+              width={64}
+              height={64}
+              className="h-full w-full object-cover size-8 rounded-full overflow-hidden"
+            />
+          </Link>
         </div>
         <div className="grid flex-1 text-left text-sm leading-tight">
-          <span className="truncate font-medium">Twisty Moto</span>
+          <Link href="/">
+            <span className="truncate font-medium">Twisty Moto</span>
+          </Link>
           <Link className="w-min" href="/membership">
             <span className="truncate text-xs text-yellow-300">Non Member</span>
           </Link>
