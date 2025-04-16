866815a79afde8f0dd928f91b571fe79a4df0da1
Laurenz Guevara <laurenzguevara@outlook.com>
2025-04-16 16:43:08 +0100

    fix: Prevent register component displaying on homepage

--- src/app/(navbar)/layout.tsx
+++ src/app/(navbar)/layout.tsx
@@ -1,27 +1,14 @@
 import NavigationBar from "@/components/navigation-bar";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { isAuthenticated } = getKindeServerSession();
-  return (await isAuthenticated())
-    ? (
-      <>
-        <NavigationBar />
-        {children}
-      </>
-    )
-    : (
-      <div className="py-4">
-        To access this you must be logged in, please&nbsp;
-        <RegisterLink className="text-green-600">Login</RegisterLink>&nbsp;to
-        view it. Or alternatively&nbsp;
-        <RegisterLink className="text-green-600">Register</RegisterLink>{" "}
-        an account.
-      </div>
-    );
+  return (
+    <>
+      <NavigationBar />
+      {children}
+    </>
+  );
 }
