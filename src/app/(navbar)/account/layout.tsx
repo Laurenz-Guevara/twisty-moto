d8dbed0111dca996f323abf886ffc10467aec8d0
Laurenz Guevara <laurenzguevara@outlook.com>
2025-04-16 17:41:17 +0100

    fix: Resolve account management route not requiring authentication

--- src/app/(navbar)/account/layout.tsx
+++ src/app/(navbar)/account/layout.tsx
@@ -1,6 +1,8 @@
 import { SidebarNav } from "@/components/sidebar-nav";
 import { Separator } from "@/components/ui/separator";
 import { Metadata } from "next";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export const metadata: Metadata = {
   title: "Forms",
@@ -26,28 +28,41 @@ interface SettingsLayoutProps {
   children: React.ReactNode;
 }
 
-export default function SettingsLayout({ children }: SettingsLayoutProps) {
-  return (
-    <>
-      <div className="container mx-auto">
-        <div className="space-y-6 py-10 px-7 pb-16">
-          <div className="space-y-0.5">
-            <h2 className="text-2xl font-bold tracking-tight">
-              Account Management
-            </h2>
-            <p className="text-muted-foreground">
-              Manage your account settings and payment preferences.
-            </p>
-          </div>
-          <Separator className="my-6" />
-          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-            <aside className="lg:w-1/5">
-              <SidebarNav items={sidebarNavItems} />
-            </aside>
-            <div className="flex-1 lg:max-w-2xl">{children}</div>
+export default async function SettingsLayout(
+  { children }: SettingsLayoutProps,
+) {
+  const { isAuthenticated } = getKindeServerSession();
+  return (await isAuthenticated())
+    ? (
+      <>
+        <div className="container mx-auto">
+          <div className="space-y-6 py-10 px-7 pb-16">
+            <div className="space-y-0.5">
+              <h2 className="text-2xl font-bold tracking-tight">
+                Account Management
+              </h2>
+              <p className="text-muted-foreground">
+                Manage your account settings and payment preferences.
+              </p>
+            </div>
+            <Separator className="my-6" />
+            <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+              <aside className="lg:w-1/5">
+                <SidebarNav items={sidebarNavItems} />
+              </aside>
+              <div className="flex-1 lg:max-w-2xl">{children}</div>
+            </div>
           </div>
         </div>
+      </>
+    )
+    : (
+      <div className="py-4">
+        To access this you must be logged in, please&nbsp;
+        <RegisterLink className="text-green-600">Login</RegisterLink>&nbsp;to
+        view it. Or alternatively&nbsp;
+        <RegisterLink className="text-green-600">Register</RegisterLink>{" "}
+        an account.
       </div>
-    </>
-  );
+    );
 }
