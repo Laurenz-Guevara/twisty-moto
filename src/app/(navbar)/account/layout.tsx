e9e6efafc7f4132361e0d2b5d5b2394668fe854c
Laurenz Guevara <laurenzguevara@outlook.com>
2025-04-30 01:26:33 +0100

    chore: Clean up old auth checking in the layouts

--- src/app/(navbar)/account/layout.tsx
+++ src/app/(navbar)/account/layout.tsx
@@ -1,7 +1,5 @@
 import { SidebarNav } from "@/components/sidebar-nav";
 import { Separator } from "@/components/ui/separator";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 const sidebarNavItems = [
   {
@@ -25,38 +23,27 @@ const sidebarNavItems = [
 export default async function SettingsLayout(
   { children }: { children: React.ReactNode },
 ) {
-  const { isAuthenticated } = getKindeServerSession();
-  return (await isAuthenticated())
-    ? (
-      <>
-        <div className="container mx-auto">
-          <div className="space-y-6 py-10 px-7 pb-16">
-            <div className="space-y-0.5">
-              <h2 className="text-2xl font-bold tracking-tight">
-                Account Management
-              </h2>
-              <p className="text-muted-foreground">
-                Manage your account settings and payment preferences.
-              </p>
-            </div>
-            <Separator className="my-6" />
-            <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-              <aside className="lg:w-1/5">
-                <SidebarNav items={sidebarNavItems} />
-              </aside>
-              <div className="flex-1 lg:max-w-2xl">{children}</div>
-            </div>
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="space-y-6 py-10 px-7 pb-16">
+          <div className="space-y-0.5">
+            <h2 className="text-2xl font-bold tracking-tight">
+              Account Management
+            </h2>
+            <p className="text-muted-foreground">
+              Manage your account settings and payment preferences.
+            </p>
+          </div>
+          <Separator className="my-6" />
+          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+            <aside className="lg:w-1/5">
+              <SidebarNav items={sidebarNavItems} />
+            </aside>
+            <div className="flex-1 lg:max-w-2xl">{children}</div>
           </div>
         </div>
-      </>
-    )
-    : (
-      <div className="py-4">
-        To access this you must be logged in, please&nbsp;
-        <RegisterLink className="text-green-600">Login</RegisterLink>&nbsp;to
-        view it. Or alternatively&nbsp;
-        <RegisterLink className="text-green-600">Register</RegisterLink>{" "}
-        an account.
       </div>
-    );
+    </>
+  );
 }
--- src/app/(sidebar)/layout.tsx
+++ src/app/(sidebar)/layout.tsx
@@ -14,42 +14,29 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { isAuthenticated } = getKindeServerSession();
-  return (await isAuthenticated())
-    ? (
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <BreadcrumbNavigation />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-            </div>
-          </header>
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            {children}
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <BreadcrumbNavigation />
+            <Separator orientation="vertical" className="mr-2 h-4" />
           </div>
-        </SidebarInset>
-      </SidebarProvider>
-    )
-    : (
-      <div className="py-4">
-        To access this you must be logged in, please&nbsp;
-        <RegisterLink className="text-green-600">Login</RegisterLink>&nbsp;to
-        view it. Or alternatively&nbsp;
-        <RegisterLink className="text-green-600">Register</RegisterLink>&nbsp;
-        an account.
-      </div>
-    );
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
 
 function BreadcrumbNavigation() {
