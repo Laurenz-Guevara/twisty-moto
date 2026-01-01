11deb58912b978bd1ae223f93c6ae2bd402cd4df
Laurenz Guevara <laurenzguevara@outlook.com>
2026-01-01 10:12:30 +0000

    TM-47: Add convert to GPX button and convert it

--- src/components/route-editor-components/route-actions.tsx
+++ src/components/route-editor-components/route-actions.tsx
@@ -17,10 +17,59 @@ import { defaultRoute, useRouteStore } from "@/app/stores/useRouteStore";
 import { saveRoute } from "@/db/database";
 import { ToastVariant } from "@/db/enums";
 import { toast } from "sonner";
+import GeoJsonToGpx from "@dwayneparton/geojson-to-gpx"
+import { Feature, LineString, Position } from 'geojson';
+import { getRouteServerSideRoutes } from "@/db/database";
 
 export default function RouteActions() {
   const updateRouteState = useRouteStore((s) => s.updateRouteData);
   const route = useRouteStore((s) => s);
+
+  async function handleExportRoute() {
+    const gpxRoute = await getRouteServerSideRoutes(route.routeJson)
+    const flatCoordinates: Position[] = [];
+
+    for (const leg of gpxRoute[0].legs) {
+      for (const step of leg.steps) {
+        if (step.geometry?.coordinates) {
+          for (const coord of step.geometry.coordinates) {
+            flatCoordinates.push(coord as Position);
+          }
+        }
+      }
+    }
+
+    const options = {
+      metadata: {
+        name: route.routeName,
+      }
+    }
+    const geojson: Feature<LineString> = {
+      type: "Feature",
+      properties: {
+        name: route.routeLocation,
+      },
+      geometry: {
+        type: "LineString",
+        coordinates: flatCoordinates,
+      },
+    };
+
+    const gpx = GeoJsonToGpx(geojson, options);
+    const gpxString = new XMLSerializer().serializeToString(gpx);
+
+    const blob = new Blob([gpxString], { type: 'application/gpx+xml' });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `${route.routeName}.gpx`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(url);
+  }
 
   function handleResetRoute() {
     updateRouteState({
@@ -107,10 +156,19 @@ export default function RouteActions() {
                 <span>Clear Route</span>
               </SidebarMenuSubButton>
             </SidebarMenuSubItem>
+            <SidebarMenuSubItem>
+              <SidebarMenuSubButton
+                className="hover:cursor-pointer"
+                onClick={() => handleExportRoute()}
+              >
+                <RefreshCcw className="h-4 w-4" />
+                <span>Export to GPX file</span>
+              </SidebarMenuSubButton>
+            </SidebarMenuSubItem>
+
           </SidebarMenuSub>
         </CollapsibleContent>
       </SidebarMenuItem>
     </Collapsible>
   );
 }
-
