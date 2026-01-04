1e226d7f91405b57a57e350d475620f7013cf4d8
Laurenz Guevara <laurenzguevara@outlook.com>
2026-01-04 12:12:50 +0000

    TM-57: Add border to sidebar header

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -15,7 +15,7 @@ import {
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
-      <SidebarHeader>
+      <SidebarHeader className="border-b">
         <SidebarHeaderContent />
       </SidebarHeader>
       <SidebarContent>
