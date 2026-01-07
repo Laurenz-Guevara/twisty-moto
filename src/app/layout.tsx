9e842bb5477a7e1a5c9d039c7cf8897e374d2a33
Laurenz Guevara <laurenzguevara@outlook.com>
2026-01-07 12:59:04 +0000

    TM-68: Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { Toaster } from "@/components/ui/sonner";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <Toaster richColors />
+          <Footer />
         </Providers>
       </body>
     </html>
